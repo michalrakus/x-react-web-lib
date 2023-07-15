840de1299b5765786ad40bb7c77e2e62ec915a35
michalrakus <michalrakus@gmail.com>
2023-07-15 15:10:50 +0200

    verzia 1.7.1 - aj pre Save button sa vola metoda formReadOnly

--- src/components/XFormFooter.tsx
+++ src/components/XFormFooter.tsx
@@ -2,12 +2,17 @@ import React from "react";
 import {XFormBase} from "./XFormBase";
 import {XButton} from "./XButton";
 
+// constant to be used in method formReadOnly to identify save button
+export const xSaveButtonId: string = "x-save-button-id";
+
 // helper wrapper
 export const XFormFooter = (props: {form: XFormBase;}) => {
 
+    const readOnly = props.form.formReadOnlyBase(xSaveButtonId);
+
     return (
         <div className="flex justify-content-center">
-            <XButton label="Save" onClick={props.form.onClickSave}/>
+            <XButton label="Save" onClick={props.form.onClickSave} disabled={readOnly}/>
             <XButton label="Cancel" onClick={props.form.onClickCancel}/>
         </div>
     );
