a02299601aacdc26acffe3e6e02bd99e3ed15f8a
michalrakus <michalrakus@gmail.com>
2024-05-24 11:40:02 +0200

    verzia 1.24.1 - pridany prop

--- src/components/XFormHeader.tsx
+++ src/components/XFormHeader.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import {XFormBase} from "./XFormBase";
 import {xLocaleOption} from "./XLocale";
 
-export const XFormHeader = (props: {form?: XFormBase; label: string;}) => {
+export const XFormHeader = (props: {form?: XFormBase; label: string; appendNewRow: boolean;}) => {
 
     return (
-        <div className="x-form-header">{props.label + (props.form?.isAddRow() ? " - " + xLocaleOption('newRow') : "")}</div>
+        <div className="x-form-header">{props.label + (props.appendNewRow && props.form?.isAddRow() ? " - " + xLocaleOption('newRow') : "")}</div>
     );
 }
+
+XFormHeader.defaultProps = {
+    appendNewRow: true
+};
