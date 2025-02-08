e86bbc211d6bafeca7439a1dc72a5ca80c04ebb9
michalrakus <michalrakus@gmail.com>
2025-02-08 23:16:28 +0100

    verzia 1.32.6 - upraveny XEditorBase, zvysena verzia PrimeReact

--- src/components/XEditorBase.tsx
+++ src/components/XEditorBase.tsx
@@ -1,6 +1,67 @@
 import React, {useState} from "react";
 import {XUtils} from "./XUtils";
 import {Editor, EditorTextChangeEvent} from "primereact/editor";
+import Quill from 'quill';
+
+// bez tohto kodu size options nefunguju
+const Size: any = Quill.import('attributors/style/size');
+Size.whitelist = ['10px', '12px', '14px', '16px', '18px', '24px', '32px', '48px'];
+Quill.register(Size, true);
+
+const Font: any = Quill.import('formats/font');
+Font.whitelist = [
+    'sans-serif', 'serif', 'monospace',
+    'times-new-roman', 'comic-sans',
+];
+Quill.register(Font, true);
+
+const headerTemplate = () => {
+    // poznamka - k tomuto layout-u sa este viazu css styly umiestnene v App.css
+    const styleMarginRight: React.CSSProperties = {marginRight: '0.7rem'};
+    return (
+        <span className="ql-formats">
+
+            <select className="ql-font" aria-label="Font">
+                <option value="sans-serif" selected>Sans-Serif</option>
+                <option value="serif">Serif</option>
+                <option value="monospace">Monospace</option>
+                <option value="times-new-roman">TimesNewRoman</option>
+                <option value="comic-sans">Comic Sans</option>
+            </select>
+
+            <select className="ql-size" aria-label="Font Size" style={styleMarginRight}>
+                <option value="10px">10</option>
+                <option value="12px" selected>12</option>
+                <option value="14px">14</option>
+                <option value="16px">16</option>
+                <option value="18px">18</option>
+                <option value="24px">24</option>
+                <option value="32px">32</option>
+                <option value="48px">48</option>
+            </select>
+
+            <button className="ql-bold" aria-label="Bold"></button>
+            <button className="ql-italic" aria-label="Italic"></button>
+            <button className="ql-underline" aria-label="Underline"></button>
+            <button className="ql-strike" aria-label="Strikethrough" style={styleMarginRight}></button>
+
+            <select className="ql-color" aria-label="Text Color"></select>
+            <select className="ql-background" aria-label="Background Color" style={styleMarginRight}></select>
+
+            <select className="ql-align" aria-label="Text Alignment" style={styleMarginRight}>
+                <option selected></option>
+                <option value="center"></option>
+                <option value="right"></option>
+            </select>
+
+            <button className="ql-list" value="ordered" aria-label="Ordered List"></button>
+            <button className="ql-list" value="bullet" aria-label="Bullet List"></button>
+            <button className="ql-indent" value="-1" aria-label="Indent to the left"></button>
+            <button className="ql-indent" value="+1" aria-label="Indent to the right"></button>
+
+        </span>
+    );
+};
 
 // koli optimalizacii - typovany text si zapisuje do svojej stavovej premennej a onChange zavola az z onBlur
 // pri velkych formularoch je totiz volanie zmeny stavu this.setState({object: this.state.object, errorMap: this.state.errorMap}); pomale
@@ -48,6 +109,7 @@ export const XEditorBase = (props: {
 
     return (
         <Editor id={props.id} value={getValue()} onTextChange={onTextChange} onBlur={onBlur} readOnly={props.readOnly}
-                style={props.style} {...XUtils.createTooltipOrErrorProps(props.error)}/>
+                pt={{root: {style: props.style}}}
+                {...XUtils.createTooltipOrErrorProps(props.error)} headerTemplate={headerTemplate()}/>
     );
 }
